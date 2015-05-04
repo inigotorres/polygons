77081031f55355b49757567f0e7fb7c13e79ded5
itorres <itorres@peertransfer.com>
2015-05-04 16:52:44 +0200

    unvalid triangle test written RED

--- spec/polygons_spec.rb
+++ spec/polygons_spec.rb
@@ -13,5 +13,8 @@ describe "A polygon" do
     it "can't be created if number of sides is not three" do 
       expect{Triangle.new(sides: [1,2,3,4])}.to raise_error
     end
+    it "can't be created if the sum of two sides is shorter than the third" do
+      expect{Triangle.new(sides: [1,1,5])}.to raise_error
+    end
   end
 end
