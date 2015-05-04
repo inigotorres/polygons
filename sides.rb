6de5636aea234687f2b28cc4f62f217ebd286f93
itorres <itorres@peertransfer.com>
2015-05-04 15:41:43 +0200

    created class side and its tests

---
+++ sides.rb
@@ -0,0 +1,12 @@
+class Side
+  attr_reader :length
+ 
+  def initialize length
+    @length = length
+    raise 'Side length must be positive' unless valid?
+  end
+
+  def valid?
+    @length > 0
+  end
+end
---
+++ spec/side_spec.rb
@@ -0,0 +1,11 @@
+require './sides'
+
+describe "A side" do
+  it "can be created and has property length" do
+    a_side = Side.new(3)
+    expect(a_side.length).to eq(3)
+  end
+  it "is not created if its side is 0 or negative" do
+    expect{Side.new(-1)}.to raise_error
+  end
+end
