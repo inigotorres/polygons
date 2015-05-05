f4e3fce878c0e8486657d6e2355a59a978404494
itorres <itorres@peertransfer.com>
2015-05-05 13:39:46 +0200

    can properly create rectangle GREEN and REFAC

--- polygons.rb
+++ polygons.rb
@@ -80,11 +80,7 @@ class Triangle < Polygon
 
   private
   def acceptable_sides?
-    is_acceptable = true
-    is_acceptable = false if @a > (@b+@c)
-    is_acceptable = false if @b > (@a+@c)
-    is_acceptable = false if @c > (@a+@b)   
-    is_acceptable 
+    (@a < @b+@c) && (@b < @a+@c) && (@c < @a+@b) 
   end
 end
 
@@ -100,3 +96,31 @@ class Square < Polygon
   end
 end
 
+class Rectangle < Polygon
+  attr_reader :base_length, :height_length
+
+  def valid?
+    raise "A rectangle must have four sides" if number_of_sides != 4
+    raise "A rectangle can't be regular" if regular?
+    raise "A rectangle must have equal opposite sides" if !acceptable_sides?
+  end
+
+  def post_initialize
+    sides_in_order = @sides.sort{|side| side.length}
+    @base_length = sides_in_order.first.length
+    @height_length = sides_in_order.last.length
+  end
+
+  private
+  def acceptable_sides?
+    base_counter = 0
+    height_counter = 0
+
+    @sides.each do |side|
+      base_counter += 1 if side.length == @base_length
+      height_counter += 1 if side.length == @height_length
+    end
+
+    (base_counter == 2) && (height_counter == 2)
+  end
+end
--- spec/polygons_spec.rb
+++ spec/polygons_spec.rb
@@ -58,4 +58,25 @@ describe "A polygon" do
       expect(a_square.area).to eq(1)
     end
   end
+
+  context "when it is a rectangle" do
+    a_rectangle = Rectangle.new(sides: [2,2,1,1])
+
+    it "can be created" do
+      expect(a_rectangle.convert_sides_to_array).to eq([2,2,1,1])
+    end
+    it "can't be created if it doesn't have four sides" do
+      error_message = "A rectangle must have four sides"
+      expect{Rectangle.new(sides: [2,2,3,3,3])}.to raise_error(error_message)
+    end
+    it "can't be regular" do
+      error_message = "A rectangle can't be regular"
+      expect{Rectangle.new(sides: [1,1,1,1])}.to raise_error(error_message)
+    end
+    it "can't be created if it doesn't have equal opposite sides" do
+      error_message = "A rectangle must have equal opposite sides"
+      expect{Rectangle.new(sides: [2,1,2,2])}.to raise_error(error_message)
+      expect{Rectangle.new(sides: [2,2,3,4])}.to raise_error(error_message)
+    end
+  end
 end
